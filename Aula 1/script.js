6b576c28daf28031db1cb5e0f9755fa290191a84
tiagow8 <tiagogomed81@gmail.com>
2023-07-04 18:44:16 -0300

    Recursos Nativos

---
+++ Aula 1/script.js
@@ -0,0 +1,10 @@
+//console
+console.log('Boa noite pessoal')
+console.info('Console de informação')
+console.warn('cuidado!')
+console.error('aqui temos uma mensagem de erro!')
+
+//caixas de dialogo
+window.alert('janela de laerta')
+wimdow.confirm('tudo bem pessoal?')
+window.prompt('qual seu nome?')
